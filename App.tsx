e2ee5a61bd36e3478c45f316f2c40614f89f3b69
Hershul Singh <hershulsingh55@gmail.com>
2024-01-19 00:22:43 +0530

    Made good UI

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const App = () =>{
   return (
-    <View>
-      <Text>Hello WOrld</Text>
+    <View style={{gap:8,flex:1,backgroundColor:"#DBE7C9",alignItems:'center'}}>
+      <Image style={{width:"50%",height:"18%",marginTop:5}} source={require('./src/image/ARITHMETIC.jpg')} />
+      <Text style={{color:'#294B29',fontSize:30}}>Grade Calculator</Text>
+      <View style={{flexDirection:"column",gap:20,width:"100%",alignItems:'center'}}>
+        <TextInput style ={{width:"90%",backgroundColor:'#789461',height:"10%",justifyContent:'center',borderRadius:20}}/>
+        <TextInput style ={{width:"90%",backgroundColor:'#789461',height:"10%",justifyContent:'center',borderRadius:20}}/>
+        <TextInput style ={{width:"90%",backgroundColor:'#789461',height:"10%",justifyContent:'center',borderRadius:20}}/>
+        <TextInput style ={{width:"90%",backgroundColor:'#789461',height:"10%",justifyContent:'center',borderRadius:20}}/>
+        <TextInput style ={{width:"90%",backgroundColor:'#789461',height:"10%",justifyContent:'center',borderRadius:20}}/>
+
+        <TouchableOpacity style ={{backgroundColor:'white',borderRadius:50,borderWidth:1,height:"10%",justifyContent:'center',alignItems:'center',width:"30%"}}>
+          <Text style={{color:'black',fontSize:20}}>Calculate</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
